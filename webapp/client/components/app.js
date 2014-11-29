bcebe0d1767abb0f979a8520fbd7f93709c306e9
Rafael Chiti <rafaelchiti@gmail.com>
2014-11-29 14:44:51 -0300

    Adding the first iteration of design

--- webapp/client/components/app.js
+++ webapp/client/components/app.js
@@ -12,23 +12,47 @@ module.exports = React.createClass({
   },
 
   renderQuestion: function(question) {
-    return <div>
-      <h1>{question}</h1>
-      <p>
-        <button onClick={this.handleYes}>Yes</button>
-        <button onClick={this.handleNo}>No</button>
-      </p>
+    return <div className="question">
+      <div className="question_mainVerticalLine"></div>
+      <div className="question_mainHorizontalLine"></div>
+      <h1 className="question_text">{question}</h1>
+
+      <div className="question_mainActionsContainer">
+        <div className="mainActions_line"></div>
+        <a className="mainAction positiveAction  " onClick={this.handleYes}>YES</a>
+        <a className="mainAction negativeAction  " onClick={this.handleNo}>NO</a>
+      </div>
     </div>;
   },
 
   renderCode: function(code) {
     var statusCode = State.getStatusCode(this.props.state, code);
-    return <div>
-      <h1>This is your status code: {code} - {statusCode.get('name')}</h1>
-      <p>
-        {statusCode.get('description')}
-      </p>
-    </div>
+    return (
+      <div className="answer">
+        <div className="answer_mainVerticalLine"></div>
+
+        <div className="answer_textContainer">
+          <div className="_text_titleContainer">
+            <div className="_titleContainer_mainHorizontalLine"></div>
+            <h1 className="_titleContainer_code">{code}</h1>
+            <span className="_titleContainer_shortDescription">{statusCode.get('name')}</span>
+          </div>
+          <p className="_text_description">
+            {statusCode.get('description')}
+          </p>
+        </div>
+
+        <div className="answer_mainActionsContainer">
+          <div className="mainActions_line"></div>
+          <a className="mainAction neutralAction" onClick={this.handleRestart}>RESTART</a>
+        </div>
+      </div>
+    );
+  },
+
+  handleRestart: function(ev) {
+    // TODO: Refactor this to use a proper router when we add one.
+    window.location = '/';
   },
 
   handleYes: function(ev) {
--- webapp/client/components/page.js
+++ webapp/client/components/page.js
@@ -12,6 +12,7 @@ module.exports = React.createClass({
   render: function() {
     return <html lang="en">
       <head>
+        <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'/>
         <script type="text/javascript" src={assetPath('bundle.js')} />
         <script type="text/javascript">start()</script>
         <title>Which status code should I use?</title>
